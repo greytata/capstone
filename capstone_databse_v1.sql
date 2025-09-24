81ceabf4d979c79b147abc0f69c424f0ccdc3c76
greytata <greytata@163.com>
2025-09-24 15:54:30 +0800

    database structure

--- capstone_databse_v1.sql
+++ capstone_databse_v1.sql
@@ -33,3 +33,26 @@ DROP CONSTRAINT fk_enrolment_foe;
 --- only keep csp table ---
 DROP TABLE IF EXISTS raw.international_fee_schedule_2024;
 DROP TABLE IF EXISTS raw.domestic_feepaying_price_2024;
+
+--- drop agreement table ---
+DROP TABLE IF EXISTS raw.funding_agreement_2024 CASCADE;
+
+---alter table csp_allocation_2024 ---
+ALTER TABLE raw.csp_allocation_2024
+DROP COLUMN IF EXISTS grandfathered_max_student_contribution,
+DROP COLUMN IF EXISTS grandfathered_commonwealth_contribution,
+DROP COLUMN IF EXISTS special_course_typepcode,
+DROP COLUMN IF EXISTS maximum_student_contribution_indicator;
+
+
+select * from raw.csp_allocation_2024;
+select * from enrolment_2024;
+
+--- checking the missing special case---
+
+SELECT r.*
+FROM raw.student_enrolments_2024 r
+LEFT JOIN dim.foe d ON r.unit_primary_foe_code = d.foe_code
+WHERE d.foe_code IS NULL;
+
+
